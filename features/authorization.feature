d1028aba171a7233a23cb9bff5342c0aba92584b
Sven Bohm <bohms@msu.edu>
2011-12-11 18:48:00 -0500

    worked on the authorization tests

--- features/authorization.feature
+++ features/authorization.feature
@@ -4,14 +4,14 @@ Feature: Reading a document in the NIS
     Given an eml document with read access given to "<user>" in the root element is inserted by "<submitter>" 
     And I am logged in as "<reader>"
     When I read the document
-    Then the result should be "<output>"
+    Then it <output>
     Examples:
       |submitter|user   |reader  | output  |
-      |ucarrol  |public |ucarrol |success  |
-      |ucarrol  |public |bohms   |success  |
-      |ucarrol  |public |public  |success  |
-      |ucarrol  |ucarrol|ucarrol |success  |
-      |ucarrol  |bohms  |ucarrol |success  |
-      |ucarrol  |ucarrol|bohms   |forbidden|
-      |ucarrol  |bohms  |public  |forbidden|
-      |ucarrol  |ucarrol|public  |forbidden|
+      |ucarrol  |public |ucarrol |succeeds  |
+      |ucarrol  |public |bohms   |succeeds  |
+      |ucarrol  |public |public  |succeeds  |
+      |ucarrol  |ucarrol|ucarrol |succeeds  |
+      |ucarrol  |bohms  |ucarrol |succeeds  |
+      |ucarrol  |ucarrol|bohms   |fails with forbidden|
+      |ucarrol  |bohms  |public  |fails with forbidden|
+      |ucarrol  |ucarrol|public  |fails with forbidden|
--- features/step_definitions/reader_steps.rb
+++ features/step_definitions/reader_steps.rb
@@ -1,9 +1,8 @@
 require File.expand_path('../lib/eml.rb', __FILE__)
 
 Given /^an eml document with read access given to "([^"]*)" in the root element is inserted by "([^"]*)"$/ do |reader, owner|
-  pending # express the regexp above with the code you wish you had
-  eml =  Eml.new
-  Nokogiri::XML::Builder.with(eml.root) do |xml|
+  @eml =  EML.create
+  Nokogiri::XML::Builder.with(@eml.doc) do |xml|
     xml.access {
       xml.allow{
         xml.principal reader
@@ -11,15 +10,21 @@ Given /^an eml document with read access given to "([^"]*)" in the root element 
       }
     }
   end
+  @eml.validate.should be_true, 'eml document is not valid'
 
+  resource = RestClient::Resource.new('http://pasta.lternet.edu/gatekeeper/package/eml', :user=>"uid=#{owner},o=LTER,dc=ecoinformatics,dc=org", :password => password)
+  @res = resource.post(@eml.to_xml, :content_type => 'application/xml') {|response, request, result| response }
+  @res.code.should  == 200
 end
 
-Given /^I am logged in as "([^"]*)"$/ do |arg1|
-    pending # express the regexp above with the code you wish you had
+Given /^I am logged in as "([^"]*)"$/ do |user|
+  @user = user
 end
 
 When /^I read the document$/ do
-    pending # express the regexp above with the code you wish you had
+  resource = RestClient::Resource.new("http://pasta.lternet.edu/gatekeeper/package/eml/#{@eml.scope}/#{@eml.id}/#{@eml.rev}", :user=>"uid=#{user},o=LTER,dc=ecoinformatics,dc=org", :password => password)
+  @res = resource.get {|response, request, result| response }
+  @res.code.should == 200
 end
 
 Then /^the result should be "([^"]*)"$/ do |arg1|
