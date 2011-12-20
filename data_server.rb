563fb16d629e4b501abcf1436706ca54c93f48c2
Sven Bohm <bohms@msu.edu>
2011-12-20 18:16:34 -0500

    added stuff to make the webserver slow

--- data_server.rb
+++ data_server.rb
@@ -4,11 +4,17 @@ class DataServer < Sinatra::Base
   enable :sessions
 
   @@desired = ""
+  @@sleep = 0
 
   get '/' do
+    sleep @@sleep
     @@desired.empty? ? "1\n" : @@desired
   end
   post '/' do 
     @@desired = request.body.read
+  end
+
+  post '/sleep' do
+    @@sleep = request.body.read.to_i
   end
 end
